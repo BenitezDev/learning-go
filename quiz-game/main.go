cc47da0b51d9ba435f0b727e5ec2e6eb7e2aa606
BenitezDev <benitezdev@gmail.com>
2021-11-25 15:56:37 +0100

    Refactor and clean up

--- quiz-game/main.go
+++ quiz-game/main.go
@@ -7,6 +7,14 @@ import (
 	"strings"
 )
 
+func printHelp() {
+	fmt.Println("Usage of the quiz game:")
+	fmt.Println("\t-csv string")
+	fmt.Println("\t\ta csv file in the format of 'question,answer' (default \"problems.csv\")")
+	fmt.Println("\t-limit int")
+	fmt.Println("\t\tthe time limit for the quiz in seconds (default 30)")
+}
+
 func main() {
 
 	helpPtr := flag.Bool("h", false, "display help options")
@@ -15,11 +23,7 @@ func main() {
 	flag.Parse()
 
 	if *helpPtr {
-		fmt.Println("Usage of the quiz game:")
-		fmt.Println("\t-csv string")
-		fmt.Println("\t\ta csv file in the format of 'question,answer' (default \"problems.csv\")")
-		fmt.Println("\t-limit int")
-		fmt.Println("\t\tthe time limit for the quiz in seconds (default 30)")
+		printHelp()
 		return
 	}
 
@@ -36,19 +40,13 @@ func main() {
 
 	for i := 0; i < totalQuestions; i++ {
 
-		fmt.Print(qa[i].Question, "=")
+		fmt.Printf("Problem #%d: %s=", i+1, qa[i].Question)
 
 		_, _ = fmt.Scanln(&input)
 
 		if strings.Compare(qa[i].Answer, input) == 0 {
 			hits++
-			fmt.Print("  Correct!")
-		} else {
-			fmt.Print("  Incorrect!")
 		}
-
-		fmt.Println(" (", hits, "/", totalQuestions, ") hits")
-		input = ""
 	}
-
+	fmt.Printf("You scored %d out of %d.\n", hits, totalQuestions)
 }
